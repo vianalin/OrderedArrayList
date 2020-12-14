b28dd9a30b5be0f4cc30f60006cc903e403e1a17
vianalin <vlin21@stuy.edu>
2020-12-13 21:33:30 -0500

    tested add(t)

--- Tester.java
+++ Tester.java
@@ -4,14 +4,14 @@ public class Tester {
 		NoNullArrayList<Integer> a = new NoNullArrayList<>();
 		
 		a.add(1);
-		a.set(0, 2);
+		//a.set(0, 2);
 		a.add(4);
-		a.add(1, 6);
+		//a.add(1, 6);
 		
 
 		//a.add(null); //testing add(t) null
 		//a.set(1, null); //testing set null
-		a.add(3, null); //testing add(index, t) null
+		//a.add(3, null); //testing add(index, t) null
 	
 		for(int i : a) {
 			System.out.println(i);
