5c14c9d8710d0eb6bd9c977837ac97db01aa01ad
vianalin <vlin21@stuy.edu>
2020-12-13 20:13:23 -0500

    tested add(index, t)

--- Tester.java
+++ Tester.java
@@ -6,9 +6,12 @@ public class Tester {
 		a.add(1);
 		a.set(0, 2);
 		a.add(4);
+		a.add(1, 6);
+		
 
 		//a.add(null); //testing add(t) null
-		a.set(1, null); //testing set null
+		//a.set(1, null); //testing set null
+		a.add(3, null); //testing add(index, t) null
 	
 		for(int i : a) {
 			System.out.println(i);
